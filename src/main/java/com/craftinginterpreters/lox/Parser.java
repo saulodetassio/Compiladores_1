ca734939a04af037b59ed68143fdbe554582d82e
saulo <saulodetassio@gmail.com>
2025-06-22 22:39:57 -0300

    Up date parser

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -5,6 +5,7 @@ import java.util.List;
 import static main.java.com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+  private static class ParseError extends RuntimeException {}
   private final List<Token> tokens;
   private int current = 0;
 
@@ -81,6 +82,7 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+    throw error(peek(), "Expect expression.");
   }
     private boolean match(TokenType... types) {
     for (TokenType type : types) {
@@ -91,6 +93,11 @@ class Parser {
     }
 
     return false;
+  }
+    private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
   }
     private boolean check(TokenType type) {
     if (isAtEnd()) return false;
@@ -110,5 +117,37 @@ class Parser {
 
   private Token previous() {
     return tokens.get(current - 1);
+  }
+    private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
+    Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
   }
 }
